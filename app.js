edafc48b0ebf8d01975deb3b3be01bfbde09c147
salmaalbarwani <salmaalbarwani45@gmail.com>
2025-07-15 12:50:00 +0400

    2

--- app.js
+++ app.js
@@ -79,4 +79,4 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+module.exports = app; 
